29a44dfca7b522972a478c3fc2de155b8789e6fc
MacwithaQ <maqallaf@gmail.com>
2022-01-06 19:37:06 +0300

    bmi challenge until 2 complete

--- index.js
+++ index.js
@@ -7,5 +7,34 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
+
   // You are going to write the code here before the curly braces 👇🏻
+  height = height/100;
+  let bmi = weight/(height^2);
+
+  if(bmi<18.5){
+
+    alert(`Your bmi is ${bmi} and you are under weight.`)
+
+  }
+
+  else if(bmi<=24.9){
+
+    alert(`Your bmi is ${bmi} and you have a healthy weight!`)
+
+  }
+
+  else if(bmi<=29.9){
+
+    alert(`Your bmi is ${bmi} and you are overweight.`)
+
+  }
+
+  else if(bmi>=30.0){
+
+    alert(`Your bmi is ${bmi} and you are obese.`)
+
+  }
+
+
 }
