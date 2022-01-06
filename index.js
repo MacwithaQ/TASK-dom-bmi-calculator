a00678597ec7ad8de2816b81bb0365e2a84cb2d6
MacwithaQ <maqallaf@gmail.com>
2022-01-06 20:14:10 +0300

    added some notes from teachers solutions

--- index.js
+++ index.js
@@ -9,30 +9,52 @@ function calculateBMI() {
 
 
   // You are going to write the code here before the curly braces 👇🏻
+
   height = height/100;
   let bmi = weight/(height^2);
 
-  if(bmi<18.5){
-
-    alert(`Your bmi is ${bmi} and you are under weight.`)
-
-  }
-
-  else if(bmi<=24.9){
+  if((age>=19 && age<=24) && (bmi>=19 && bmi<=24)){
 
     alert(`Your bmi is ${bmi} and you have a healthy weight!`)
 
   }
 
-  else if(bmi<=29.9){
+  else if((age>=25 && age<=34) && (bmi>=20 && bmi<=25)){
 
-    alert(`Your bmi is ${bmi} and you are overweight.`)
+    alert(`Your bmi is ${bmi} and you have a healthy weight!`)
 
   }
 
-  else if(bmi>=30.0){
+  else if((age>=35 && age<=44) && (bmi>=21 && bmi<=26)){
 
-    alert(`Your bmi is ${bmi} and you are obese.`)
+    alert(`Your bmi is ${bmi} and you have a healthy weight!`)
+
+  }
+
+  else if((age>=45 && age<=54) && (bmi>=22 && bmi<=27)){
+
+    alert(`Your bmi is ${bmi} and you have a healthy weight!`)
+
+  }
+
+  else if((age>=55 && age<=64) && (bmi>=23 && bmi<=28)){
+
+    alert(`Your bmi is ${bmi} and you have a healthy weight!`)
+
+  }
+
+  else if((age>=65) && (bmi>=24 && bmi<=29)){
+
+    alert(`Your bmi is ${bmi} and you have a healthy weight!`)
+
+  }
+  
+  else{
+
+// You dont necessarily need to use \n to create spacing when using string interpolation. You do need it if youre doing strings with ""
+
+    alert(`Your bmi is ${bmi} and you are overweight. \n Age:${age} \n Height:${height*100} \n Weight:${weight}`
+    )
 
   }
 
